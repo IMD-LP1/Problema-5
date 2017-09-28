53fc741af1cad26fb1fb1e6ed4a7b7069b7455a6
Cleydson Talles Araujo Vieira <talles17@A307-32L.imd.local>
2017-09-28 20:23:16 -0300

    questao2-incompleta

--- lists/conjuntos.cpp
+++ lists/conjuntos.cpp
@@ -15,6 +15,10 @@ public:
 	void write() ;
 	void read() ;
 	int get_tamanho() ;
+	void set_tamanho(int) ;
+	T get_elemento(int) ;
+	void insert(T ele, int pos) ;
+	bool pertinencia(int) ;
 
 private:
 	T *elementos ;
@@ -41,7 +45,7 @@ void conjunto<T>::write() {
 		cin >> elementos[ii] ;
 		if (ii > 0) {
 			for (int jj{0} ; jj < ii ; jj++) {
-				if (elementos[ii] == elementos[jj]){
+				if (pertinencia(ii)){
 					cout << "O elemento " << elementos[ii] << " ja esta no conjunto, digite outro." << endl ;
 					ii-- ;
 				}
@@ -50,16 +54,26 @@ void conjunto<T>::write() {
 	}
 }
 
-//template<class T>
-//conjunto<T>::tirando_ele_iguais
+template<class T>
+bool conjunto<T>::pertinencia(int tam_atual) {
+	for (int i{0} ; i < tam_atual ; i++) {
+		if (elementos[i] == elementos[tam_atual]){	
+			return true ;
+		}
+	}
+	return false ;
+}
 
 template<class T>
 void conjunto<T>::read() {
 	cout << "{" ;
 	for (int i{0} ; i < tamanho ; i++) {
-		cout << elementos[i] << ", " ; 
 		if (i+1 == tamanho) {
 			cout << elementos[i] << "}\n" ;
+			i++ ;
+		}
+		else {	
+			cout << elementos[i] << ", " ; 
 		}
 	}
 }
@@ -70,17 +84,60 @@ int conjunto<T>::get_tamanho() {
 }
 
 template<class T>
+void conjunto<T>::set_tamanho(int t) {
+	tam = t ;
+}
+
+template<class T>
+T conjunto<T>::get_elemento(int pos;) {
+	return elementos[pos] ;
+}
+
+template<class T>
+void conjunto<T>::insert(T ele, int pos) {
+	for (int i{0} ; i < pos ; i++) {
+		if (!pertinencia(pos)) {
+			elementos[pos] = ele ;
+		}	
+	}
+}
+
+template<class T>
 conjunto<T>::~conjunto() {
 	delete[] elementos ;
 }
 
-/*template<class T>
-conjunto conjunto<T>::uniao (conjunto a, conjunto b ) {
-	tamanho = a.get_tamanho() + b.get_tamanho() ;
+template<class T>
+bool conjunto<T>::belongs(T ele) {
+	for (int i{0} ; i < tam ; i++) {
+		if (ele == elementos[i])  {
+			return true ;
+		}
+	}
+	return false ; 
+}
 
+template<class T>
+conjunto conjunto<T>::intersecao (conjunto a) {
+	conjunto <T>b ;
+	if (tam < a.get_tamanho()) {
+		b.set_tamanho(tam) ;
+	}
+	else {
+		b.set_tamanho(a.get_tamanho()) ;
+	}
+	int indice{0} ;
+	for (int i{0} ; i < b.get_tamanho() ; i++) {
+		if(a.belongs(elementos[i])){
+			b.insert(elementos[i],indice ) ;
+			indice++ ;	
+		}
+	}
+	return b ;
 
-} */
+	
 
+}	
 
 int main () {
 	int na , nb ;
@@ -102,6 +159,9 @@ int main () {
 	cout << "B = " ; 
 	b.read() ;
 
+	conjunto <int>c = a.intersecao(b) ;
+	cout << "A inter B = " ;
+	c.read() ;
 
 
 
