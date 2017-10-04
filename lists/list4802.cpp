8f7ec9cceeda691d141accf08a55bf9463ebaf6a
talles <talles17.a@hotmail.com>
2017-10-04 17:16:54 -0300

    questao 3 incompleta

---
+++ lists/list4802.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+
+template <class T>
+class rational
+{
+public :
+	typedef T value_type ;
+	rational() : rational{0} {}
+	rational(T num) : numerator_{num} , denominator_ {1} {}
+	rational(T num , T den) ;
+
+	void assign (T num, T den) ; 
+
+	template<class U>
+	U convert()
+	const {
+		return static_cast<U>(numerator()) / static_cast<U>(denominador()) ;
+	}
+
+	T numerator() const { return numerator_;}
+	T denominador() const { return denominator_ ;}
+
+private:
+	void reduce() ;
+	T numerator_ ;
+	T denominator_ ;
+} ;
+
+
+template <class T>
+rational<T>::rational(T num, T den) : numerator_{num} , denominator_{den} {
+	rational<T>::reduce() ;
+}
+
+template <class T>
+void rational<T>::assign(T num , T den) {
+	numerator_ = num ; 
+	denominator_ = den ;
+	rational<T>::reduce() ;
+}
+
+template <class T>
+void rational<T>reduce() {}
+
+template<class T>
+bool operator==(rational<T> const& a, rational<T> const& b){
+	return a.numerator() == b.numerator() and a.denominator() == b.denominator() ;
+}
+
+template <class T>
+inline bool operator!=(rational<T> const& a, rational<T> const& b) {
+	
+}
