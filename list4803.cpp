44ae9873fff220f121f5aed4ea216a604880506d
Fernando <fernandomaciel@ufrn.edu.br>
2017-10-05 20:17:52 -0300

    Add files via upload

--- list4803.cpp
+++ list4803.cpp
@@ -21,6 +21,7 @@ public:
     T numerator() const { return numerator_; }
     T denominator() const { return denominator_; }
 private:
+    T MDC();
     void reduce();
     T numerator_;   // numerator gets tje sing of the rational value.
     T denominator_; // denominator is always positive
@@ -45,7 +46,24 @@ void rational<T>::assign(T num, T den)
 
 // Reduce the numerator and denominator by their GCD.
 template<class T>
-void rational<T>::reduce(){}
+void rational<T>::reduce(){
+rational<T>::MDC();
+
+}
+template<class T>
+T rational<T>::MDC(){
+
+    int resto;
+    resto = numerator_% denominator_;
+              while(resto!=0)
+              {
+                  numerator_    = denominator_;
+                  denominator_    = resto;
+                  resto = numerator_ % denominator_;         
+              }
+              return denominator_;
+              //system("pause");
+}
 
 //  Compare two rational number for equality.
 template<class T>
@@ -65,9 +83,12 @@ int main() {
     rational<short> zero{};
     rational<int> pi1{355, 113}, x{1, 2}, y{2, 4};
     rational<long> pi2{80143857L, 25510582L};
+    rational<int> r{(rational<int>)3 * rational<int>{1, 3}};
 
+    std::cout <‌< r.numerator() <‌< ’/’ <‌< r.denominator() <‌< std::endl;
+ 
 
-    if(x == y)
+    if(bool operator==() != bool operator!=() )
         std::cout << "1/2 == 2/4" << std::endl;
     else
         std::cout << "1/2 != 2/4" << std::endl;
